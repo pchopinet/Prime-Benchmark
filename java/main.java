562f7a2c876c692235daf1b3d723c9af007c95a4
Pierre Chopinet <pierre.chopinet97@gmail.com>
2017-09-07 17:16:16 +0200

    Add multithreading to java benchmark

--- java/main.java
+++ java/main.java
@@ -12,21 +12,47 @@ public class main {
         return true;
     }
 
-    public static void boucle_nb_premier(int start_number, int max, int pas) {
-        int j = 0;
+    public static void boucle_nb_premier(int start_number, int max, int pas, int j[]) {
+        int jj = 0;
         for (int i = start_number; i < max; i += pas) {
             if (est_premier(i)) {
-                j++;
+                jj++;
+                //System.out.println(i);
             }
         }
-        System.out.println("Il y a " + j + " nombres premier entre " + start_number + " et " + max);
+        j[0] += jj;
     }
 
+    public static void go(int max, int nombre_thread) {
+        int j[] = new int[1];
+        j[0] = 0;
+        int start_number = 1;
+        System.out.println(max);
+        Thread tab_t[] = new Thread[nombre_thread];
+        for (int i = 0; i < nombre_thread; i++) {
+            int k = i;
+
+            tab_t[i] = new Thread(() -> boucle_nb_premier(k, max, nombre_thread, j));
+            tab_t[i].start();
+        }
+
+        try {
+            for (int i = 0; i < nombre_thread; i++) {
+                tab_t[i].join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Il y a " + j[0] + " nombres premier entre " + start_number + " et " + max);
+
+    }
 
     public static void main(String[] args) {
         long debut = System.currentTimeMillis();
 
-        boucle_nb_premier(1, 100000000, 1);
+        go(100000000, 8);
+
 
         long fin = System.currentTimeMillis();
         double temps = (fin - debut) / 1000;
