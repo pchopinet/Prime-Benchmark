a9cc7caacf138cec7e3e801295d8fa86a745fec8
Pierre Chopinet <pierre.chopinet97@gmail.com>
2017-09-20 22:23:04 +0200

    Add multithreading to python benchmark (very slow)

--- py/main.py
+++ py/main.py
@@ -1,6 +1,7 @@
 import time
 from math import sqrt
 from threading import Thread
+import queue
 
 
 def is_prime(nombre):
@@ -13,13 +14,13 @@ def is_prime(nombre):
     return True
 
 
-def loop_prime(start_number, max, pas, j):
+def loop_prime(start_number, max, pas, q):
     jj = 0
     for i in range(start_number, max, pas):
         if is_prime(i):
             jj += 1
     print(jj)
-    j += jj
+    q.put(jj)
 
 
 def go(max, number_thread):
@@ -27,20 +28,25 @@ def go(max, number_thread):
     print("Prime Benchmark :", max)
 
     tab_t = []
-    for i in range(number_thread):
-        k = i
-        t = Thread(target=loop_prime, args=(k, max, number_thread, j))
+    q = queue.Queue()
+    for i in range(0, number_thread * 2, 2):
+        k = i + 1
+        t = Thread(target=loop_prime, args=(k, max, number_thread * 2, q))
         t.start()
         tab_t.append(t)
 
     for i in range(number_thread):
         tab_t[i].join()
+
+    while not q.empty():
+        j += q.get()
+
     print("There are", j, "prime numbers between", 1, "and", max)
 
 
 start_time = time.time()
 
-go(100000, 8)
+go(100000000, 8)
 
 end_time = time.time()
 time_exec = end_time - start_time
