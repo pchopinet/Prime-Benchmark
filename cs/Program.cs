2afb12b9ebdd0aa937369f79fce6050669b2d72f
pchopinet <pierre.chopinet97@gmail.com>
2018-02-03 13:26:13 +0100

    delete spaces

--- cs/Program.cs
+++ cs/Program.cs
@@ -5,14 +5,12 @@ namespace cs
 {
     class Program
     {
-
         static Boolean is_prime(int number)
         {
             if (number % 2 == 0)
                 return false;
 
             double max = Math.Sqrt(number) + 1;
-
             for (int i = 3; i < max; i += 2)
             {
                 if (number % i == 0)
@@ -51,21 +49,18 @@ namespace cs
                 tab_t[i / 2].Start();
             }
 
-            
                 for (int i = 0; i < number_thread; i++)
                 {
                     tab_t[i].Join();
             }
-
-
-            Console.WriteLine("There are " + j + " prime numbers between " + 1 + " and " + max);
-
+            Console.WriteLine("There are " +
+             j + " prime numbers between " + 1 + " and " + max);
         }
 
         static void Main(string[] args)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            
+
             go(Int32.Parse(args[0]), Int32.Parse(args[1]));
             watch.Stop();
             var elapsedMs = watch.ElapsedMilliseconds;
