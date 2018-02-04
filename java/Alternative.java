5085f0f716b47334f9e5213666e496094f506c94
pchopinet <pierre.chopinet97@gmail.com>
2018-02-04 10:31:06 +0100

    some experimentation with alternative

--- java/Alternative.java
+++ java/Alternative.java
@@ -1,22 +1,25 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Created by Pierre on 15/09/2017.
  */
 public class Alternative {
 
-    public static boolean is_prime(int number, ArrayList<Integer> tab, int taille) {
+    public static boolean is_prime(int number, ConcurrentLinkedQueue<Integer> tab) {
 
         if (number % 2 == 0)
             return false;
         int i = 0;
         double max = Math.sqrt(number) + 1;
-        /*for (i = 0; i < taille && i < max; i++) {
-            if (number % tab.get(i) == 0) {
-                return false;
-            }
-        }*/
-        for (int p : tab) {
+
+        Iterator<Integer> iter = tab.iterator();
+        int p;
+        while (iter.hasNext()) {
+            p = iter.next();
+
             if (number % p == 0) {
                 return false;
             } else if (i > max) {
@@ -27,11 +30,11 @@ public class Alternative {
         return true;
     }
 
-    public static void loop_prime(int start_number, int max, int pas, int j[], ArrayList<Integer> tab) {
+    public static void loop_prime(int start_number, int max, int pas, int j[], ConcurrentLinkedQueue<Integer> tab) {
         int jj = 0;
 
         for (int i = start_number; i < max; i += pas) {
-            if (is_prime(i, tab, jj + 1)) {
+            if (is_prime(i, tab)) {
                 tab.add(i);
                 jj++;
                 //System.out.println(i);
@@ -46,12 +49,17 @@ public class Alternative {
         j[0] = 0;
         System.out.println("Prime Benchmark : " + max);
         Thread tab_t[] = new Thread[number_thread];
-        ArrayList<Integer> tab = new ArrayList<>();
-        tab.add(2);
-        for (int i = 0; i < number_thread; i++) {
-            int k = i;
 
-            tab_t[i] = new Thread(() -> loop_prime(3, max, number_thread, j, tab));
+        ConcurrentLinkedQueue<Integer> tab = new ConcurrentLinkedQueue<>();
+
+        tab.add(2);
+        tab.add(3);
+        tab.add(5);
+        tab.add(7);
+
+        for (int i = 0; i < number_thread; i++) {
+            int k = i + 7;
+            tab_t[i] = new Thread(() -> loop_prime(k, max, number_thread, j, tab));
             tab_t[i].start();
         }
 
@@ -63,7 +71,6 @@ public class Alternative {
             e.printStackTrace();
         }
 
-        System.out.println(tab.get(2));
         System.out.println("There are " + j[0] + " prime numbers between " + 1 + " and " + max);
 
     }
